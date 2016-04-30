713b4c0f68957fc6502428ee6c848dcf881b79eb
quarbby <lynnette-ng@outlook.com>
2016-04-30 09:05:18 +0800

    Added input box

--- 02_instagrampage/js/app.js
+++ 02_instagrampage/js/app.js
@@ -1,36 +1,32 @@
+var feed;
+var category = "penguins";
+
 $(function() {
+    loadFeed(); 
+});
+
+function onBtnClick() {
+    category = $(".input").val().toLowerCase();
+}
+
+function loadFeed() {
+    
+    // Clear div
+    $("#_instafeed").html("");
  
     //Set up instafeed
     var imgs = [];
-    var feed = new Instafeed({
+    feed = new Instafeed({
         clientId: 'f2259c6e745d4df087e9d4659325ae97',
         //target: 'instafeed',
         get: 'tagged',
-        tagName: 'cats',
+        tagName: category,
         links: true,
         limit: 3,
         sortBy: 'most-recent',
         success: function(images) {
             
-            //console.log(images.data);
-            /*
-            $.ajax({
-                url: "https://api.projectoxford.ai/vision/v1.0/describe?maxCandidates=1",
-                beforeSend: function(xhrObj){
-                    // Request headers
-                    xhrObj.setRequestHeader("Content-Type","application/json");
-                    xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","bc6e814e6a3c4db595d49fb5ec045ee7");
-                },
-                type: "POST",
-                async: false,
-                data: '{ "url": "https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/13098967_164793917250826_685040446_n.jpg?ig_cache_key=MTIzOTExNDM1OTY4NTAyMzkwNA%3D%3D.2.l" }',
-                success: function(data) {
-                    successCB(data);
-                }
-            });
-            */
-            
-            var captions = {'desc': '', 'confidence': 0};
+            var captions = {'desc': '', 'confidence': ''};
             
             function successCB(data) {
                 console.log(data);
@@ -39,7 +35,6 @@ $(function() {
                 //captions.push([desc, confidence]); 
                 captions['desc'] = desc;
                 captions['confidence'] = (confidence*100).toFixed(2);   
-                console.log("Confidence " + confidence);         
             }
             
             //console.log("Desc " + captions);
@@ -81,6 +76,8 @@ $(function() {
                     msdesc: captions['desc']
                 });
                 imgs.push(result);
+                //console.log(result);
+                //$('#_instafeed').append(result);
             }
             $('#_instafeed').html(imgs);
         },
@@ -94,6 +91,17 @@ $(function() {
     });
     feed.run();
  
+}
+
+/*==== INFINITE SCROLLING ======*/
+
+$(window).scroll(function(){
+  if($(window).scrollTop() + $(window).height() > $(document).height() - 100) 
+    {
+      feed.next();
+    }
+  
 });
 
 // MS cognitive vision key: bc6e814e6a3c4db595d49fb5ec045ee7
+// Other valid instagram client ID: 2e5a3f8cc1754723a81d216f3d302a69
