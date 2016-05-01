7a08de5dcbb655c5a4051980d1e845258202cc8a
quarbby <lynnette-ng@outlook.com>
2016-05-01 17:26:22 +0800

    Add styles to modal

--- 02_instagrampage/js/app.js
+++ 02_instagrampage/js/app.js
@@ -39,7 +39,7 @@ function loadFeed(category) {
     //Set up instafeed
     var imgs = [];
     feed = new Instafeed({
-        clientId: 'f2259c6e745d4df087e9d4659325ae97',
+        clientId: 'x',
         //target: 'instafeed',
         get: 'tagged',
         tagName: category,
@@ -88,7 +88,7 @@ function loadFeed(category) {
                     beforeSend: function(xhrObj){
                         // Request headers
                         xhrObj.setRequestHeader("Content-Type","application/json");
-                        xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","bc6e814e6a3c4db595d49fb5ec045ee7");
+                        xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","x");
                     },
                     type: "POST",
                     async: false,
@@ -212,5 +212,4 @@ $(window).scroll(function(){
   
 });
 
-// MS cognitive vision key: bc6e814e6a3c4db595d49fb5ec045ee7
 // Other valid instagram client ID: 2e5a3f8cc1754723a81d216f3d302a69
