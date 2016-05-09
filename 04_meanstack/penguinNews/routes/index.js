77a3f856870524a38030b94d8ab661afefa5fdcb
Lynnette Ng <lynnette-ng@outlook.com>
2016-05-09 13:30:07 +0000

    Add comments routing

--- 04_meanstack/penguinNews/routes/index.js
+++ 04_meanstack/penguinNews/routes/index.js
@@ -42,14 +42,54 @@ router.param('post', function(req, res, next, id){
 });
 
 router.get('/posts/:post', function(req, res){
-    res.json(req.post);
+    req.post.populate('comments', function(err, post) {
+       if (err) { return next(err); }
+       res.json(post);
+    });
 });
 
-router.get('/posts/:post/upvote', function(req, res, next) {
+router.put('/posts/:post/upvote', function(req, res, next) {
    req.post.upvote(function(err, post){
       if (err) {return next(err); }
       res.json(post);
    });
+});
+
+/* Comments */
+
+router.param('comment', function(req, res, next, id){
+   var query = Comment.findById(id);
+   
+   query.exec(function (err, comment){
+        if (err) { return next(err); }
+        if (!comment) { return next(new Error('Cant\'t find comment')); }
+        
+        req.comment = comment;
+        return next();
+   });
+});
+
+
+router.post('/posts/:post/comments', function(req, res, next) {
+   var comment = new Comment(req.body);
+   comment.post = req.post;
+   
+   comment.save(function(err, comment){
+      if (err) { return next(err); }
+      
+      req.post.comments.push(comment);
+      req.post.save(function(err, post) {
+         if (err) {return next(err); }
+         res.json(comment);
+      });
+   });
+});
+
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next){
+   req.comment.upvote(function(err, comment){
+      if (err) { return next(err); }
+      res.json(comment);
+   }); 
 });
 
 module.exports = router;
