2348183818af05380d632a1fbbe969f06368c642
Lynnette Ng <lynnette-ng@outlook.com>
2016-05-09 23:41:28 +0000

    Working barebones app

--- 04_meanstack/README.md
+++ 04_meanstack/README.md
@@ -1,6 +1,12 @@
 **Following**: 
 Learn to Build Modern Web Apps with MEAN by Thinkster [link](https://thinkster.io/mean-stack-tutorial)
 
+**To Run**
+```
+$./mongod &
+$npm start
+```
+
 **Further Resources**
 * [Setting up a database](https://docs.c9.io/docs/setup-a-database#mongodb)
 * [Setting up MongoDB on Cloud9 Workspace](https://community.c9.io/t/setting-up-mongodb/1717)
@@ -9,4 +15,10 @@ $ mkdir data
 $ echo 'mongod --bind_ip=$IP --dbpath=data --nojournal --rest "$@"' > mongod
 $ chmod a+x mongod
 $ ./mongod
+```
+* [Repairing MongodDB](http://stackoverflow.com/questions/12831939/couldnt-connect-to-server-127-0-0-127017)
+```
+$sudo rm data/mongod.lock
+$sudo mongod --repair
+$./mongod
 ```
--- 04_meanstack/penguinNews/public/javascripts/angularApp.js
+++ 04_meanstack/penguinNews/public/javascripts/angularApp.js
@@ -24,6 +24,26 @@ app.config([
                         return posts.get($stateParams.id);
                     }]
                 }
+            })
+            .state('login', {
+                url: '/login',
+                templateUrl: '/login.html',
+                controller: 'AuthCtrl',
+                onEnter: ['$state', 'auth', function($state, auth){
+                    if(auth.isLoggedIn()) {
+                        $state.go('home');
+                    }
+                }]
+            })
+            .state('register', {
+                url: '/register',
+                templateUrl: '/register.html',
+                controller: 'AuthCtrl',
+                onEnter: ['$state', 'auth', function($state, auth){
+                    if(auth.isLoggedIn()) {
+                        $state.go('home');
+                    }
+                }]
             });
         $urlRouterProvider.otherwise('home');
     }
@@ -80,7 +100,7 @@ app.factory('auth', ['$http', '$window', function($http, $window){
 }
 ]);
 
-app.factory('posts', ['$http', function($http){
+app.factory('posts', ['$http', 'auth', function($http, auth){
     var o = {
         posts: []
     };
@@ -90,12 +110,16 @@ app.factory('posts', ['$http', function($http){
       });
     };
     o.create = function(post) {
-      return $http.post('/posts', post).success(function(data){
+      return $http.post('/posts', post, {
+          headers: {Authorization: 'Bearer ' + auth.getToken() }
+      }).success(function(data){
          o.posts.push(data); 
       });
     };
     o.upvote = function(post) {
-      return $http.put('/posts/' + post._id + '/upvote').success(function(data){
+      return $http.put('/posts/' + post._id + '/upvote', null, {
+          headers: {Authorization: 'Bearer ' + auth.getToken() }
+      }).success(function(data){
             post.upvotes += 1;
         });
     };
@@ -105,10 +129,14 @@ app.factory('posts', ['$http', function($http){
       });
     };
     o.addComment = function(id, comment) {
-      return $http.post('/posts/' + id + '/comments', comment);  
+      return $http.post('/posts/' + id + '/comments', comment, {
+          headers: {Authorization: 'Bearer ' + auth.getToken() }
+      });  
     };
     o.upvoteComment = function(post, comment){
-      return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote')
+      return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote', null, {
+          headers: {Authorization: 'Bearer ' + auth.getToken() }
+      })
         .success(function(data){
           comment.upvotes += 1;
         });
@@ -119,8 +147,10 @@ app.factory('posts', ['$http', function($http){
 app.controller('MainCtrl', [
    '$scope', 
    'posts',
-   function($scope, posts) {
+   'auth',
+   function($scope, posts, auth) {
        $scope.posts = posts.posts;
+       $scope.isLoggedIn = auth.isLoggedIn;
        $scope.addPost = function(){
             if (!$scope.title || $scope.title === '') { return; }
             posts.create({
@@ -137,12 +167,10 @@ app.controller('MainCtrl', [
    }
 ]);
 
-app.controller('PostsCtrl', [
-    '$scope',
-    'posts',
-    'post',
-    function($scope, posts, post){
+app.controller('PostsCtrl', ['$scope', 'posts', 'post', 'auth',
+    function($scope, posts, post, auth){
         $scope.post = post;
+       $scope.isLoggedIn = auth.isLoggedIn;
         $scope.addComment = function () {
             if ($scope.body === '') { return; }
             posts.addComment(post._id, {
@@ -157,5 +185,37 @@ app.controller('PostsCtrl', [
         $scope.incrementUpvotes = function (comment) {
             posts.upvoteComment (post, comment);
         };
+    }
+]);
+
+app.controller('AuthCtrl', ['$scope', '$state', 'auth',
+    function($scope, $state, auth){
+        $scope.user = {};
+        
+        $scope.register = function(){
+          auth.register($scope.user).error(function(error){
+              $scope.error = error;
+          }).then(function(){
+              $state.go('home');
+          });  
+        };
+        
+        $scope.logIn = function(){
+          auth.logIn($scope.user).error(function(error){
+              $scope.error = error;
+          }).then(function(){
+              $state.go('home');
+          });          
+        };
+        
+    }
+]);
+
+app.controller('NavCtrl', [
+    '$scope', 'auth',
+    function($scope, auth){
+        $scope.isLoggedIn = auth.isLoggedIn;
+        $scope.currentUser = auth.currentUser;
+        $scope.logOut = auth.logOut;
     }
 ]);
