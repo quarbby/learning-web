9fafb931a8b50e1bcea3a95cb2926547c372d1cd
Lynnette Ng <lynnette-ng@outlook.com>
2016-05-10 11:45:19 +0000

    Add flip card class

--- 05_memorychallenge/js/main.js
+++ 05_memorychallenge/js/main.js
@@ -3,6 +3,7 @@
 */
 
 var tiles;
+var timer;
 
 // Stats
 var wrong;
@@ -13,14 +14,31 @@ $(function(){
     startTimer();
     initialiseTiles();
     setUpGame();
+    
+    $('#start-game').click(function() {
+       setUpGame();
+       startTimer();
+    });
+
+    
+    $('#game-board img').click(function() {
+        var clickedImg = $(this);
+        var tile = clickedImg.data('tile'); 
+        if (tile.flipped) {
+            return;
+        } else {
+            flipTile(tile, clickedImg);
+        }
+    });
 });
 
 /* 
 * Starts the elapsed time timer
 */
 function startTimer() {
+    clearInterval(timer);
     var startTime = Date.now();
-    var timer = window.setInterval(function() {
+    timer = window.setInterval(function() {
         var secondsElapsed = Math.floor((Date.now() - startTime) / 1000);
         $('#elapsed-seconds').text(secondsElapsed + ' seconds');
     }, 1000);
@@ -35,7 +53,7 @@ function initialiseTiles() {
     var numTiles = 32;
     tiles = [];  // Array of tile objects
     
-    for (var i=0; i <= 32; i++) {
+    for (var i=1; i <= 32; i++) {
         tiles.push({
             tileNum: i,
             src: 'img/tile' + i + '.jpg',
@@ -54,6 +72,15 @@ function resetStats() {
     remaining = 8;
 }
 
+/* 
+* Display statistics 
+*/ 
+
+function displayStats() {
+    $('#matches').text(matches);
+    $('#remaining').text(remaining);
+    $('#wrong').text(wrong);
+}
 
 /*
 * Sets up and displays game board
@@ -61,6 +88,7 @@ function resetStats() {
 
 function setUpGame() {
     resetStats();
+    displayStats();
     
     tiles = _.shuffle(tiles);
     
@@ -82,14 +110,55 @@ function drawGame(tilePairs) {
     gameBoard.empty();
     
     var row = $(document.createElement('div'));
+    //row.addClass('flip-container');
     _.forEach(tilePairs, function(tile, idx){
         if ((idx % 4) == 0) {
             // Every 4 tiles create new row
             gameBoard.append(row);
             row = $(document.createElement('div'));
+            //row.addClass('flip-container');
         }
         
-        // Create tile images
+        var flipContainer = $(document.createElement('span'));
+        flipContainer.addClass('flip-container');
+
+        var flipperDiv = $(document.createElement('span'));
+        flipperDiv.addClass('flipper');
+        flipperDiv.addEventListener('click', function(){
+                   $(this).toggleClass("flipped"); }, false);
+
+        var frontDiv = $(document.createElement('span'));
+        frontDiv.addClass('front');
+        
+        var frontImg = $(document.createElement('img'));
+        frontImg.attr({
+           src: 'img/tile-back.png',
+           alt: 'placeholder'
+        });
+
+        frontImg.appendTo(frontDiv);
+
+        var backDiv = $(document.createElement('span'));
+        backDiv.addClass('back');
+        
+        var backImg = $(document.createElement('img'));
+        backImg.attr({
+           src: 'img/tile' + tile.tileNum + '.jpg',
+           alt: 'tile ' + tile.tileNum
+        });
+        backImg.data('tile', tile);
+        
+        backImg.appendTo(backDiv);
+
+        frontDiv.appendTo(flipperDiv);
+        backDiv.appendTo(flipperDiv);
+        
+        flipperDiv.appendTo(flipContainer);
+        
+        row.append(flipContainer);
+
+        /* 
+        // Simple image appending
         var img = $(document.createElement('img'));
         img.attr({
            src: 'img/tile-back.png',
@@ -97,7 +166,15 @@ function drawGame(tilePairs) {
         });
         img.data('tile', tile);
         row.append(img);
+        */
     });
     
     gameBoard.append(row);
+}
+
+/*
+* Flips the tile 
+*/
+function flipTile(tile, img) {
+    
 }
